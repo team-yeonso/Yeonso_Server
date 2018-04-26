87eb886201f35932175bb118665596b2e1a8a8d5
syeutyu <syeuty@naver.com>
2018-04-26 16:27:41 +0900

    test

--- src/test/java/com/yenso/yensoserver/YensoserverApplicationTests.java
+++ src/test/java/com/yenso/yensoserver/YensoserverApplicationTests.java
@@ -1,16 +1,11 @@
 package com.yenso.yensoserver;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = YensoserverApplication.class)
 public class YensoserverApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
 
 }
