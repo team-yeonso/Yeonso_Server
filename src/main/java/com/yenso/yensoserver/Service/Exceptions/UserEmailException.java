eb5852bcbfd887f7a5a9b309be1ad12541563471
syeutyu <syeuty@naver.com>
2018-05-15 13:09:32 +0900

    [Exception] Email Exception 추가

--- src/main/java/com/yenso/yensoserver/Service/Exceptions/UserEmailException.java
+++ src/main/java/com/yenso/yensoserver/Service/Exceptions/UserEmailException.java
@@ -1,4 +1,8 @@
 package com.yenso.yensoserver.Service.Exceptions;
 
-public class UserEmailException {
+public class UserEmailException extends Exception {
+    public UserEmailException(String text){
+        super(text);
+    }
+
 }
