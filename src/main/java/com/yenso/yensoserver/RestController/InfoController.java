3553447f096c99500388ca81dd003aeee601e06e
syeutyu <syeuty@naver.com>
2018-06-05 23:56:17 +0900

    [ADD] Info Controller Base

--- src/main/java/com/yenso/yensoserver/RestController/InfoController.java
+++ src/main/java/com/yenso/yensoserver/RestController/InfoController.java
@@ -1,4 +1,26 @@
 package com.yenso.yensoserver.RestController;
 
+import com.yenso.yensoserver.Repository.InfoRepo;
+import com.yenso.yensoserver.Service.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RequestMapping(value = "/Info")
+@RestController
 public class InfoController {
+
+    @Autowired
+    @Qualifier(value = "infoRepo")
+    private InfoRepo infoRepo;
+
+    @RequestMapping("/insert")
+    public void insertData(@RequestBody Map<String,Object> data, @RequestHeader(value = "Authorization")String token){
+        String jwt = Jwt.filterToken(token);
+    }
 }
